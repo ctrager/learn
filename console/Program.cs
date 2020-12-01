9392ace749826b64fb5fbd17e5ea781d44a85f30
corey <ctrager@yahoo.com>
2020-12-01 17:47:25 -0600

    adding a bunch

---
+++ console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var dict = new Dictionary<string, dynamic>();
+        int[] array = null; // dict["x"];
+        if (array is null)
+            Console.WriteLine("corey says null");
+        else
+            Console.WriteLine(array);
+
+    }
+
+}
